4f39025415418af368352818a77742f93577a7e5
ivangag <igagliot@actiaitalia.com>
2014-02-27 17:50:56 +0100

    Init Commit

---
+++ TouchGestures/build/source/buildConfig/debug/course/examples/touch/Gestures/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package course.examples.touch.Gestures;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "course.examples.touch.Gestures";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
